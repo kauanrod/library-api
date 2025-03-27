fe1a9707367c4942e467fb94627c86985575aa2b
Kauan Rodrigues <kauanrod@hotmail.com>
2025-03-27 20:01:15 -0300

    Configuração inicial do projeto

---
+++ src/main/java/kauanrod/libraryapi/LibraryapiApplication.java
@@ -0,0 +1,13 @@
+package kauanrod.libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryapiApplication.class, args);
+	}
+
+}
