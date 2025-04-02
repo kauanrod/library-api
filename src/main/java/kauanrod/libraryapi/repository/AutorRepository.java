c493bcea7098e676d9ed0a4bd60a796580e36527
Kauan Rodrigues <kauanrod@hotmail.com>
2025-04-02 08:58:05 -0300

    Criando o pacote repository

---
+++ src/main/java/kauanrod/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,11 @@
+package kauanrod.libraryapi.repository;
+
+import kauanrod.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
---
+++ src/main/java/kauanrod/libraryapi/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package kauanrod.libraryapi.repository;
+
+import kauanrod.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
+}
